175bc7286dfd2f60f929d19f590d80e8014160ed
xiaoyangLee <lixiaoyang2020@gmail.com>
2017-08-04 17:00:49 +0800

    add new files

---
+++ reptile/install/module.sql
@@ -0,0 +1,2 @@
+INSERT INTO `phpcms_module` (`module`, `name`, `url`, `iscore`, `version`, `description`, `setting`, `listorder`, `disabled`, `installdate`, `updatedate`)
+VALUES ('reptile', '新闻爬虫', '', '0', '1.0', '', '', '0', '0', '2017-07-26', '2017-07-26');
---
+++ reptile/install/reptile.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `v9_reptile`(
+   `id` INT UNSIGNED AUTO_INCREMENT,
+   `title` VARCHAR(100) NOT NULL,
+   `author` VARCHAR(40) NOT NULL,
+   `submission_date` DATE,
+   PRIMARY KEY ( `id` )
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
