d24793b233d5be01adef3d8c040dc0cd9c618fdd
xiaoyangLee <lixiaoyang2020@gmail.com>
2017-08-16 17:20:28 +0800

    add new file

--- reptile/functions/SqlHelper.php
+++ reptile/functions/SqlHelper.php
@@ -1,6 +1,6 @@
 <?php 
 	/**
-	* 
+	* SqlHelper 数据库操作类
 	*/
 	class SqlHelper
 	{
@@ -8,14 +8,31 @@
 		{
 			$mysqli = new mysqli('127.0.0.1', 'root', '','phpcmsv9');
 			if ($mysqli->connect_error) {
-				die('Connect Error (' . $mysqli->connect_errno . ') '. $mysqli->connect_error);
+				die('Connect Error (' . $mysqli->connect_error() . ') '. $mysqli->connect_error());
 			}
 			//设置utf-8编码
 			$mysqli->query('SET NAMES utf8');
 
-			if ($result = $mysqli->query("SELECT id,title from v9_python")
+			if ($result = $mysqli->query("SELECT id,title,inputtime,url from v9_python"))
 			{
-				
+				while($row=$result->fetch_array()){	
+   					echo "<tr>";
+   			
+   					echo "<td>";
+   					echo $row[0];
+  					echo "</td>";
+  					echo "<td>";
+   					echo "<a href=".$row[3]." target='_blank'>".$row[1]."</a>";
+  					echo "</td>";
+
+  					echo "<td>";
+   					echo date('r',$row[2]);
+  					echo "</td>";
+  					echo "</tr>";
+  					//echo "------------------------------------------------------";
+  				}
+  				$result->close();
 			}
+			$mysqli->close();
 		}
 	}
